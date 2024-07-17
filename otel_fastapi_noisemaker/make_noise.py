17408c4090f2ce66df144b795959f5c53f616705
James Hodgkinson <james@terminaloutcomes.com>
2024-07-17 14:02:49 +1000

    more concurrency and error handling

--- otel_fastapi_noisemaker/make_noise.py
+++ otel_fastapi_noisemaker/make_noise.py
@@ -2,13 +2,14 @@ import asyncio
 import os
 import random
 import sys
-from typing import Callable, Any
+from typing import Callable, Any, Dict, Optional
 
+import backoff
 import click
 import httpcore
-from httpx import AsyncClient
+from httpx import AsyncClient, Response
 from functools import wraps
-
+from loguru import logger
 import httpx
 
 from otel_fastapi_noisemaker import NoiseForm
@@ -45,6 +46,59 @@ URLS = {
 }
 
 
+@backoff.on_exception(
+    backoff.expo, [httpcore.ConnectError, httpx.ConnectError], max_tries=10
+)
+async def get_url(
+    client: AsyncClient,
+    url: str,
+    headers: Dict[str, str],
+    json: Optional[Dict[str, Any]] = None,
+    method: str = "GET",
+) -> Response:
+    return await client.request(method=method, url=url, headers=headers, json=json)
+
+
+async def client_task(
+    client_number: int,
+    requests: int,
+    request_delay: float,
+    method_list: list[str],
+    base_url: str,
+) -> None:
+    my_ip = (
+        f"127.{random.randint(1,254)}.{random.randint(1,254)}.{random.randint(1,254)}"
+    )
+    headers = {
+        "Forwarded": f"for={my_ip}",
+    }
+
+    async with AsyncClient() as client:
+        for _ in range(requests):
+            method = random.choice(method_list)
+            if method == "POST":
+                payload = NoiseForm(message=f"hello from {my_ip}").model_dump()
+            else:
+                payload = None
+
+            url = f"{base_url}{random.choice(URLS[method])}"
+            # try:
+            response = await get_url(
+                client=client,
+                method=method,
+                url=url,
+                json=payload,
+                headers=headers,
+            )
+            logger.debug(f"{my_ip=} {url=} {method=} {response.json()=}")
+            # except (httpcore.ConnectError, httpx.ConnectError) as error:
+            #     logger.error(f"Failed to connect: {url=} {method=} {my_ip=} {error}")
+            #     return
+            if request_delay > 0.0:
+                await asyncio.sleep(request_delay)
+    logger.success("Client {} done!", client_number)
+
+
 @click.command()
 @click.option(
     "-c",
@@ -64,46 +118,44 @@ URLS = {
     help=f"Comma-delimited list of HTTP methods to use, defaults to {','.join(DEFAULT_METHODS)}",
     default=",".join(DEFAULT_METHODS),
 )
-@click.option("-d", "--delay", help="Delay (seconds) between clients", default=0)
+@click.option("-d", "--delay", help="Delay (seconds) between clients", default=0.0)
+@click.option(
+    "-D", "--request-delay", help="Delay (seconds) between requests", default=0.0
+)
+@click.option("--debug", is_flag=True, help="Enable debug logging")
 @coro
-async def main(client_count: int, requests: int, methods: str, delay: int = 0) -> None:
+async def main(
+    client_count: int,
+    requests: int,
+    methods: str,
+    delay: float = 0.0,
+    request_delay: float = 0.0,
+    debug: bool = False,
+) -> None:
+
+    if not debug:
+        logger.remove()
+        logger.add(sys.stdout, level="INFO")
+
     method_list = methods.split(",")
     for method in method_list:
         if method not in URLS:
-            print(f"Invalid method: {method}")
+            logger.error(f"Invalid method: {method}")
             sys.exit(1)
     base_url = f"http://localhost:{SERVICE_PORT}"
-    async with AsyncClient() as client:
-        for _ in range(0, client_count):
-            # for each client, we generate an IP address and the "Forwarded" header
-            my_ip = f"127.{random.randint(1,254)}.{random.randint(1,254)}.{random.randint(1,254)}"
-            headers = {
-                "Forwarded": f"for={my_ip}",
-            }
-            # let's do some requestin'
-            for _ in range(0, requests):
-                method = random.choice(method_list)
-                if method == "POST":
-                    payload = NoiseForm(message=f"hello from {my_ip}").model_dump()
-                else:
-                    payload = None
 
-                url = f"{base_url}{random.choice(URLS[method])}"
-                try:
-                    response = await client.request(
-                        method=method,
-                        url=url,
-                        json=payload,
-                        headers=headers,
-                    )
-                    print(f"{my_ip=} {url=} {method=} {response.json()=}")
-                except httpcore.ConnectError as error:
-                    print(f"Failed to connect:  {url=} {method=} {my_ip=} {error}")
-                except httpx.ConnectError as error:
-                    print(f"Failed to connect:  {url=} {method=} {my_ip=} {error}")
-            if delay > 0:
-                print(f"Waiting {delay} second{'' if delay == 1 else 's'}")
-                await asyncio.sleep(delay)
+    tasks = []
+    for client_num in range(client_count):
+        logger.info(f"Creating client {client_num+1}")
+        task = asyncio.create_task(
+            client_task(client_num, requests, request_delay, method_list, base_url)
+        )
+        tasks.append(task)
+        if delay > 0:
+            logger.debug(f"Waiting {delay} second{'' if delay == 1 else 's'}")
+            await asyncio.sleep(delay)
+
+    await asyncio.gather(*tasks)
 
 
 if __name__ == "__main__":
--- run.sh
+++ run.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
+# this is the port that the FastAPI frontend will run on.
 if [ -z "${SERVICE_PORT}" ]; then
     export SERVICE_PORT="9290";
 fi
 
+# if you're running the collector elsewhere then go with that.
 export OTEL_EXPORTER_OTLP_ENDPOINT="http://localhost:4317"
 
 export SERVICE_ENVIRONMENT="noisemaker"
 export SERVICE_HOSTNAME="noisemaker"
 export OTEL_SERVICE_NAME="NoiseMaker"
 
+# turn on always-on CPU profiling
+export SPLUNK_PROFILER_ENABLED="true"
+
+
 export OTEL_RESOURCE_ATTRIBUTES="service.namespace=${SERVICE_NAMESPACE},deployment.environment=${SERVICE_ENVIRONMENT},host.name=${SERVICE_HOSTNAME}"
 
-export OTEL_PYTHON_FASTAPI_EXCLUDED_URLS="/healthcheck,/ws,/metrics"
+# these can exclude particular endpoints if you've got problematic ones like websockets or healthchecks
+# export OTEL_PYTHON_FASTAPI_EXCLUDED_URLS="/healthcheck,/ws,/metrics"
 
 # https://www.uvicorn.org/deployment/
 poetry run opentelemetry-instrument \
